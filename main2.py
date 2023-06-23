ccfb0b7ac7bce766d61a3c0501f591d4cc72afc2
gregoryreyn <javah221@gmail.com>
2023-06-23 20:05:01 +0300

    refactoring proces 4

--- main2.py
+++ main2.py
@@ -2,113 +2,104 @@ import pandas as pd
 import openpyxl
 import itertools
 
-# Переменные для записи данных
-Namber_Acts, Data_Acts, Named_work, IS, materials = [], [], [], [], []
-finel_act, finel_IS, finel_osi_otm = [], [], []
 
+# Переменные для записи данных
+namber_acts, data_acts, named_work, IS, materials = [], [], [], [], []
+finel_act, finel_IS, finel_materials, finel_osi_otm, finel_data_acts = [], [], [], [], []
+data_acts = finel_data_acts
 
 def path_file():
-    path = input('puth: ')
-    return path
+    # Определяем путь
+    path_file_read_and_write_excel = '/Users/gregoryreyn/Desktop/Общая БД.xlsm'
+    return path_file_read_and_write_excel
 
 
-def reading_data():
-    # /Users/gregoryreyn/Documents/Developer/Общая БД.xlsm
-    path_file_read_and_write_excel = path_file()
-    df = pd.read_excel(path_file_read_and_write_excel, sheet_name='БД (3)')
-    print(df)
+def reading_data(path_file_read_and_write_excel):
+    # Открываем книгу для чтения
+    df = pd.read_excel(path_file_read_and_write_excel, sheet_name='БД')
+    return df
 
 
-def parsing_data():
-    # Проверка
-    path_file_read_and_write_excel = path_file()
-    df = pd.read_excel(path_file_read_and_write_excel, sheet_name='БД (3)')
-    print(df)
+def open_book_write():
+    # открываем книгу для записи данных
+    path_file_write_excel = '/Users/gregoryreyn/Desktop/тест книга.xlsx'  # заменить на выбор
+    workbook = openpyxl.load_workbook(path_file_write_excel)
+    return workbook
+
+
+def open_list_write(workbook):
+    # выбираем лист, в который будем записывать значения
+    worksheet = workbook['Лист2']  # заменить на выбор
+    return worksheet
+
+
+def parsing_data(df):
     # парсим и записываем данные в переменные
     for index, row in df.iterrows():
-        Namber_Acts.append(row['№ акта'])
-        Data_Acts.append(row['дата акта'])
-        Named_work.append(row['1. К освидетельствованию предъявлены следующие работы:'])
+        namber_acts.append(row['№ акта'])
+        data_acts.append(row['дата акта'])
+        named_work.append(row['1. К освидетельствованию предъявлены следующие работы:'])
         IS.append(row['4. Предъявлены документы'])
         materials.append(row['3. При выполнении работ применены:'])
-    print(Namber_Acts, Data_Acts, Named_work, IS, materials)
+    return df
 
 
 def act_gen():
-    # Проверка
-    Namber_Acts = ['№0']
-    Named_work = ['устройство']
-    # генерируем акты
-    aosr, act_namber, act_name_work = 'АОСР', Namber_Acts, Named_work
+    aosr, act_namber, act_name_work = 'АОСР', namber_acts, data_acts
     for i in range(len(act_name_work)):
         finel_act.append(f"{aosr} {act_namber[i]} {act_name_work[i]}")
-    # Вывод проверки
-    print(finel_act)
-
 
 
 def is_gen():
-    # Проверка
-    my_list1 = ['устройство']
-    # генерируем схемы (разделяем схемы)
-    finel_IS = []
+    my_list1 = IS
+    finel_IS_l = finel_IS
     for item in my_list1:
         split_str = item.split(";")
-        finel_IS += [x.strip() for x in split_str]
-    # Вывод проверки
-    print(finel_IS)
+        finel_IS_l += [x.strip() for x in split_str]
+    finel_IS_l = finel_IS
+    return finel_IS
 
 
-def materials_gen():
-    # Проверка
-    materials_list = ['устройство; устройство2; устройство3; устройство4']
-    # генерируем паспорта/сертификаты (разделяем паспорта/сертификаты)
-    finel_materials = []
-    for item in materials_list:
-        split_str = item.split(";")
-        finel_materials += [x.strip() for x in split_str]
-    # Вывод проверки
-    print(finel_materials)
+# def materials_gen():
+#     # генерируем паспорта/сертификаты (разделяем паспорта/сертификаты)
+#     materials_list = materials
+#     finel_materials = [materials]
+#     for item in materials_list:
+#         split_str = item.split(";")
+#         finel_materials += [x.strip() for x in split_str]
+#     pass
 
 
 def osi_gen():
-    finel_osi_otm = []  # новый список, куда будут записываться значения
+    # finel_osi_otm = []  # новый список, куда будут записываться значения
     for item in finel_act:
         if "в осях" in item:
             new_item = item.split("в осях", 1)[1].strip()
             finel_osi_otm.append("в осях " + new_item)
 
-    print(finel_osi_otm) # вывод нового списка
 
-def all_write():
-    finel_act, finel_IS, finel_osi_otm, Data_Acts, materials = [], [], [], ['1', '2', '3', '4', '5', ], []
-
-    # открываем книгу для записи данных
-    path_file_write_excel = '/Users/gregoryreyn/Documents/Developer/Общая БД.xlsm'
-    workbook = openpyxl.load_workbook(path_file_write_excel)
-    # выбираем лист, в который будем записывать значения
-    worksheet = workbook['Реест']
+def all_write(worksheet):
+    # записываем каждое значение из списка в ячейку
     starting_row = 12  # определяем ячейку, с которой начнём записывать значения
     starting_column = 3
-
-    # заполняем пропущенные значения в Data_Acts значением "-1"
-    for write_finel_act, write_finel_IS, write_finel_materials, write_data_Acts, write_finel_osi_otm in itertools.zip_longest(
-            finel_act, finel_IS, materials, Data_Acts, finel_osi_otm, fillvalue="none"):
+    # заполняем пропущенные значения значением "-"
+    for write_finel_act, write_finel_IS, write_finel_materials, write_finel_data_acts, write_finel_osi_otm in itertools.zip_longest(
+            finel_act, finel_IS, materials, finel_data_acts, finel_osi_otm, fillvalue="-"):
         worksheet.cell(row=starting_row, column=starting_column, value=write_finel_act)
         worksheet.cell(row=starting_row, column=starting_column + 1, value=write_finel_osi_otm)
-        worksheet.cell(row=starting_row, column=starting_column + 2, value=write_data_Acts)
+        worksheet.cell(row=starting_row, column=starting_column + 2, value=write_finel_data_acts)
         worksheet.cell(row=starting_row + 1, column=starting_column, value=write_finel_IS)
         worksheet.cell(row=starting_row + 2, column=starting_column, value=write_finel_materials)
         starting_row += 3
 
 
+def main():
+    """This function is the main function that calls other functions"""
+    p1 = path_file()
+    p2 = reading_data(p1)
+    p3 = parsing_data(p2)
+    print(namber_acts, data_acts, named_work, IS, materials)
+    return p1, p2, p3
 
 
-
-# path_file()
-# reading_data()
-# parsing_data()
-# act_gen()
-# is_gen()
-# materials_gen()
-all_write()
+main()
--- tests.py
+++ tests.py
@@ -1,112 +1,52 @@
-import pandas as pd
-import openpyxl
-import itertools
-# list_workbook = print(input('Лист книги'))
-
-
-# Читаем данные
-path_file_read_and_write_excel = '/Users/gregoryreyn/Documents/Developer/ProjectPTO_2/AutoReestr(v1)/Общая БД.xlsm'
-df = pd.read_excel(path_file_read_and_write_excel, sheet_name='БД (3)')
-
-# # Открыть и активировать книгу
-# workbook = openpyxl.load_workbook(path_file_read_and_write_excel)
-# workbook_active = workbook.active
-
-# открываем книгу для записи данных
-path_file_write_excel = '/Users/gregoryreyn/Documents/Developer/ProjectPTO_2/AutoReestr(v1)/Реестр ИД.xlsx'
-workbook = openpyxl.load_workbook(path_file_write_excel)
-# выбираем лист, в который будем записывать значения
-worksheet = workbook['Реестр']
-
-
-# Переменные для записи данных
-Namber_Acts, Data_Acts, Named_work, IS, materials = [], [], [], [], []
-
-# парсим и записываем данные в переменные
-for index, row in df.iterrows():
-    Namber_Acts.append(row['№ акта'])
-    Data_Acts.append(row['дата акта'])
-    Named_work.append(row['1. К освидетельствованию предъявлены следующие работы:'])
-    IS.append(row['4. Предъявлены документы'])
-    materials.append(row['3. При выполнении работ применены:'])
-
-
-# def act_generation(aosr, act_namber, act_name_work):
-# генерируем акты
-aosr, act_namber, act_name_work = 'АОСР', Namber_Acts, Named_work
-finel_act = []
-for i in range(len(act_name_work)):
-    finel_act.append(f"{aosr} {act_namber[i]} {act_name_work[i]}")
-# act_generation(aosr, act_namber, act_name_work)
-
-# генерируем схемы (разделяем схемы)
-my_list1 = IS
-
-finel_IS = []
-
-for item in my_list1:
-    split_str = item.split(";")
-    finel_IS += [x.strip() for x in split_str]
-
-
-# генерируем паспорта/сертификаты (разделяем паспорта/сертификаты)
-# materials_list = materials
+# class Animal:
+#     def __init__(self, voice):
+#         self.voice = voice
 #
-# finel_materials = [materials]
-
-# for item in materials_list:
-#     split_str = item.split(";")
-#     finel_materials += [x.strip() for x in split_str]
-
-
-finel_osi_otm = [] # новый список, куда будут записываться значения
-for item in finel_act:
-    if "в осях" in item:
-        new_item = item.split("в осях", 1)[1].strip()
-        finel_osi_otm.append("в осях " + new_item)
-
-# print(finel_osi_otm) # вывод нового списка
+#
+# cat = Animal('Meow')
+# print(cat.voice)  # => Meow
+#
+# dog = Animal('Woof')
+# print(dog.voice)  # => Woof
 
 
-# записываем каждое значение из списка в ячейку
-starting_row = 12 # определяем ячейку, с которой начнём записывать значения
-starting_column = 3
-
-# for write_finel_act, write_finel_IS, write_finel_materials, write_data_Acts in zip(finel_act, finel_IS, materials, Data_Acts):  # проходимся каждым из 4 значений по каждому списку в кол-ве 4
-#     worksheet.cell(row=starting_row, column=starting_column, value=write_finel_act)  # Определяем стартовые точки строки, столбца, значения данных и для колонн делаем оттуп на 1 больше предыдущего
-#     # worksheet.cell(row=starting_row, column=starting_column + 1, value=write_finel_osi_otm)
-#     worksheet.cell(row=starting_row, column=starting_column + 2, value=write_data_Acts)
-#     worksheet.cell(row=starting_row + 1, column=starting_column, value=write_finel_IS)
-#     worksheet.cell(row=starting_row + 2, column=starting_column, value=write_finel_materials)
-#     starting_row += 3  # с помощью оператора += как раз таки происходит цикл, но пока хз как. Не понятно...
+def get_input():
+    """This function prompts the user for input and returns it"""
+    user_input = input("Enter a value: ")
+    return user_input
 
 
-
-# заполняем пропущенные значения в Data_Acts значением "-1"
-for write_finel_act, write_finel_IS, write_finel_materials, write_data_Acts, write_finel_osi_otm in itertools.zip_longest(finel_act, finel_IS, materials, Data_Acts, finel_osi_otm, fillvalue=""):
-    worksheet.cell(row=starting_row, column=starting_column, value=write_finel_act)
-    worksheet.cell(row=starting_row, column=starting_column + 1, value=write_finel_osi_otm)
-    worksheet.cell(row=starting_row, column=starting_column + 2, value=write_data_Acts)
-    worksheet.cell(row=starting_row + 1, column=starting_column, value=write_finel_IS)
-    worksheet.cell(row=starting_row + 2, column=starting_column, value=write_finel_materials)
-    starting_row += 3
+def convert_to_int(user_input):
+    """This function takes a string as input and converts it to an integer"""
+    num = int(user_input)
+    return num
 
 
+def apply_operation(num):
+    """This function takes an integer as input and applies an operation (e.g. multiplying by 2)"""
+    result = num * 2
+    return result
 
-workbook_save = workbook.save('Реестр ИД.xlsx')
 
-# print(Named_work)
-# проверяем написанную херню
-# path_file_excel11 = '/Users/gregoryreyn/Documents/Developer/ProjectPTO/Auto_reestr_excel_v1/Реестр ИД.xlsx'
-# df1 = pd.read_excel(path_file_excel11, sheet_name='1')
-# print(df1)
+def convert_to_str(result):
+    """This function takes an integer as input and converts it to a string"""
+    string = str(result)
+    return string
 
-print(finel_act)
-print()
-print(finel_IS)
-print()
-print(materials)
-print()
-print(finel_osi_otm)
-print()
-print(Data_Acts)
+
+def print_output(string):
+    """This function takes a string as input and prints it to the console"""
+    print("The result is:", string)
+
+
+def main():
+    """This function is the main function that calls other functions"""
+    user_input = get_input()
+    num = convert_to_int(user_input)
+    result = apply_operation(num)
+    string = convert_to_str(result)
+    print_output(string)
+
+
+# Call the main function to run the program
+main()
